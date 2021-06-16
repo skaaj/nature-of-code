e8806e5d58c0a43ee08af7d38259b613951af24c
Benjamin Denom <ben.denom@gmail.com>
2021-06-16 22:42:31 +0200

    update walker

--- sketches/01_random/walker.js
+++ sketches/01_random/walker.js
@@ -1,42 +1,83 @@
 const width = 500;
 const height = 500;
-let red;
-let blue;
-let it = 0;
-let colorDir = 1;
-let x, y;
+const barHeight = 50;
+const totalHeight = height + barHeight;
+let walkers = [];
 
 function setup() {
+    walkers = [
+        new Walker({
+            x: width / 3, y: 2 * height / 3,
+            speed: 10,
+            color: color('red')
+        }),
+        new Walker({
+            x: width / 2, y: height / 3,
+            speed: 10,
+            color: color('green')
+        }),
+        new Walker({
+            x: 2 * width / 3, y: 2 * height / 3,
+            speed: 10,
+            color: color('blue')
+        })
+    ];
+    // dom
     select("body").style("background", "#202122");
-    createCanvas(width, height);
+    // canvas
+    createCanvas(width, height + barHeight);
+    frameRate(75);
     background(0);
-    stroke(200, 25);
-    strokeWeight(10);
-    x = width / 2;
-    y = height / 2;
-    red = color('rgba(200, 0, 0, 0.5)');
-    blue = color('rgba(0, 0, 200, 0.5)');
+    strokeWeight(50);
 }
 
 function draw() {
-    const dx = random(-1, 5);
-    const dy = random(-5, 5);
+    walkers.forEach(w => {
+        w.move();
+        w.draw();
+    });
 
-    x += dx;
-    y += dy;
+    if(frameCount % 10 == 0) {
+        const fps = frameRate();
+        strokeWeight(0);
+        fill(0);
+        rect(0, height, width, barHeight);
+        fill(255);
+        text("FPS: " + fps.toFixed(0), 10, totalHeight - 10);
+        text("Framecount: " + frameCount, 10, totalHeight - 30);
+    }
+}
 
-    let current_color = lerpColor(red, blue, it / 1000);
+class Walker {
+    constructor({x, y, speed, color}) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.color = color;
+        this.color.setAlpha(255 * 0.01);
+    }
 
-    stroke(current_color);
+    draw() {
+        strokeWeight(50);
+        stroke(this.color);
+        point(this.x, this.y);
+    }
 
-    point(
-        x >= 0 ? x % width : width - x,
-        y >= 0 ? y % height : height - y
-    );
+    move() {
+        const dx = random(-1, 1);
+        const dy = random(-1, 1);
+        this.x += dx * this.speed;
+        this.y += dy * this.speed;
 
-    it += colorDir;
-
-    if(it <= 0 || it >= 1000) {
-        colorDir *= -1;
+        // Handle horizontal bounds
+        if(this.x > width)
+            this.x -= width;
+        else if(this.x < 0)
+            this.x += width;
+        // And then vertical ones
+        if(this.y > height)
+            this.y -= height;
+        else if(this.y < 0)
+            this.y += height;
     }
 }
